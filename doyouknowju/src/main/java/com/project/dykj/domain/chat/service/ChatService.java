c50a5c4059de5926b8cf7eddcfb0c902b5a86dd3
alsrudcks123-art <alsrudcks123@gmail.com>
2026-01-28 12:10:09 +0900

    chan-채팅에 시간, 스크롤기능추가

--- doyouknowju/src/main/java/com/project/dykj/domain/chat/service/ChatService.java
+++ doyouknowju/src/main/java/com/project/dykj/domain/chat/service/ChatService.java
@@ -1,8 +1,12 @@
 package com.project.dykj.domain.chat.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
 import com.project.dykj.domain.chat.dto.ChatMessageVO;
 import com.project.dykj.domain.chat.repository.ChatRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,4 +20,12 @@ public class ChatService {
         // 필요하다면 여기서 비속어 필터링 같은 로직을 추가할 수 있습니다.
         chatRepository.insertMessage(chatMessage);
     }
+    
+ // 기존 saveMessage 메서드 아래에 추가
+    public List<ChatMessageVO> getChatList() {
+        return chatRepository.selectChatList();
+    }
+    
+    
+    
 }
