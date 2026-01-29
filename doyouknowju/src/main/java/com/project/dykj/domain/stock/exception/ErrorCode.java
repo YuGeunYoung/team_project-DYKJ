2c0fd4a152a61fc0b7a8432391179b6d752e41e4
firework72 <fir3work72@gmail.com>
2026-01-29 16:59:56 +0900

    Dong : 주식 거래 실패 관련 ErrorCode 추가

--- doyouknowju/src/main/java/com/project/dykj/domain/stock/exception/ErrorCode.java
+++ doyouknowju/src/main/java/com/project/dykj/domain/stock/exception/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     NOT_ENOUGH_HOLDING(400, "보유 주식이 부족합니다."),
     NOT_FOUND_STOCK(404, "해당 종목을 찾을 수 없습니다."),
     NOT_FOUND_USER(404, "해당 사용자를 찾을 수 없습니다."),
+
+    FAIL_TO_TRADE(500, "주식 거래에 실패했습니다."),
     
     INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다.");
 
