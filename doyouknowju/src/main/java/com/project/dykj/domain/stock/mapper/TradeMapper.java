eea8d219d996fdd5a1ccebe96412c9b23aa47579
firework72 <fir3work72@gmail.com>
2026-01-30 15:13:26 +0900

    Dong : 주식 매도 로직 구현 및 application.properties에 Holding VO 인식되도록 패키지 추가

--- doyouknowju/src/main/java/com/project/dykj/domain/stock/mapper/TradeMapper.java
+++ doyouknowju/src/main/java/com/project/dykj/domain/stock/mapper/TradeMapper.java
@@ -1,6 +1,7 @@
 package com.project.dykj.domain.stock.mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.project.dykj.domain.stock.dto.req.TradeReq;
 import com.project.dykj.domain.stock.dto.res.MyTradesRes;
 import com.project.dykj.domain.stock.dto.res.TradesByStockIdRes;
+import com.project.dykj.domain.stock.vo.Holding;
 
 @Mapper
 @Repository
@@ -21,9 +23,15 @@ public interface TradeMapper {
 
     public int selectHoldingCount(TradeReq tradeReq);
 
-    public int insertHolding(TradeReq tradeReq);
+    public Optional<Holding> selectHoldingByUserIdAndStockId(TradeReq tradeReq);
 
-    public int addHolding(TradeReq tradeReq);
+    public int insertHolding(TradeReq tradeReq); // HOLDINGS 테이블에 (아이디, 종목코드) 쌍 추가
 
-    public int updateBalance(TradeReq tradeReq);
+    public int addHolding(TradeReq tradeReq); // HOLDINGS 테이블에서 (아이디, 종목코드) 쌍의 totalCount와 totalPrice 업데이트
+
+    public int deleteHolding(TradeReq tradeReq); // HOLDINGS 테이블에서 (아이디, 종목코드) 쌍 삭제
+
+    public int reduceHolding(TradeReq tradeReq); // HOLDINGS 테이블에서 (아이디, 종목코드) 쌍의 totalCount와 totalPrice 업데이트
+
+    public int updateBalance(TradeReq tradeReq); // MEMBERS 테이블에서 userId의 points 업데이트
 }
