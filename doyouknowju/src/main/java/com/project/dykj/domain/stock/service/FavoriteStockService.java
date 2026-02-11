c509a82c004726d7fcf423a381552b92012e64fa
firework72 <fir3work72@gmail.com>
2026-02-11 15:56:54 +0900

    Dong : 랭킹 테이블 groupSize 프론트엔드에서 받도록 수정 및 관심종목에 주가 정보 뜨도록 수정

--- doyouknowju/src/main/java/com/project/dykj/domain/stock/service/FavoriteStockService.java
+++ doyouknowju/src/main/java/com/project/dykj/domain/stock/service/FavoriteStockService.java
@@ -1,12 +1,15 @@
 package com.project.dykj.domain.stock.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.project.dykj.domain.stock.dto.req.UserIdAndStockIdReq;
 import com.project.dykj.domain.stock.dto.res.GetFavoriteStocksRes;
 import com.project.dykj.domain.stock.mapper.FavoriteStockMapper;
+import com.project.dykj.kis.service.StockService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FavoriteStockService {
 
+    private final StockService stockService;
     private final FavoriteStockMapper favoriteStockMapper;
 
     public int addFavorite(UserIdAndStockIdReq req) {
@@ -25,7 +29,36 @@ public class FavoriteStockService {
     }
 
     public List<GetFavoriteStocksRes> getFavoriteStocks(String userId) {
-        return favoriteStockMapper.getFavoriteStocks(userId);
+        List<GetFavoriteStocksRes> res = favoriteStockMapper.getFavoriteStocks(userId);
+
+        final int BATCH_SIZE = 30;
+        Map<String, Object> prices = new HashMap<>();
+
+        for (int i = 0; i < res.size(); i += BATCH_SIZE) {
+            int end = Math.min(i + BATCH_SIZE, res.size());
+            List<String> batchStockIds = res.subList(i, end).stream().map(GetFavoriteStocksRes::getStockId).toList();
+            Map<String, Object> batchPrices = stockService.getMultiplePrices(batchStockIds);
+            
+            prices.putAll(batchPrices);
+
+            try {
+                Thread.sleep(750);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        for (GetFavoriteStocksRes r : res) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> priceInfo = (Map<String, Object>) prices.get(r.getStockId());
+            int currentPrice = Integer.parseInt(priceInfo.get("stck_prpr").toString());
+            r.setStockPrice(currentPrice);
+            r.setStockPriceChange(Integer.parseInt(priceInfo.get("prdy_vrss").toString()));
+            r.setStockPriceChangeRate(Double.parseDouble(priceInfo.get("prdy_ctrt").toString()));
+        }
+
+        return res;
     }
 
     public int isFavorite(UserIdAndStockIdReq req) {
